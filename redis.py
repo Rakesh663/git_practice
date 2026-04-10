50f189449c01f342bfab42e38cff7534d3aa15fd
lodemrakesh <lodemrakesh851@gmail.com>
2026-04-10 14:52:39 +0530

    Main: update redis init

--- redis.py
+++ redis.py
@@ -9,9 +9,10 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 def get_redis_client():
     return redis.Redis.from_url(
         REDIS_URL,
-        decode_responses=True  # returns strings instead of bytes
+        decode_responses=True
     )
 
 
-# Singleton client (optional)
+# 👇 CHANGE THIS LINE IN MAIN
 redis_client = get_redis_client()
+print("Redis initialized from MAIN")
