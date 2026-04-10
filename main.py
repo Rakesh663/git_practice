6cc69d987a07a985cc1d69b5aeadb93674bdee1d
lodemrakesh <lodemrakesh851@gmail.com>
2026-04-10 14:29:45 +0530

    test3: update test3.py
    git push --force-with-lease origin feature/test3#

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from app.api.router import router
 from app.core.middleware import RequestContextMiddleware
 
 app = FastAPI()
+
 print("merge conflicts in main")
+
+print("merge confllicts in test3")
 
 # 🔹 Add middleware
 app.add_middleware(RequestContextMiddleware)
