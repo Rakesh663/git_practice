ceaafecf50f728402c054853371a01ecbff69c8b
lodemrakesh <lodemrakesh851@gmail.com>
2026-04-10 14:48:09 +0530

    chnages

--- main.py
+++ main.py
@@ -5,20 +5,14 @@ from app.api.router import router
 from app.core.middleware import RequestContextMiddleware
 
 app = FastAPI()
-<<<<<<< HEAD
 
 print("merge conflicts in main")
 
 print("merge confllicts in test3")
 
 # 🔹 Add middleware
-||||||| parent of 0a02c0f (Feature3: update main.py)
-print("merge confllicts in test3")
-# 🔹 Add middleware
-=======
 print("merge confllicts in test3 branch")
  # Add middleware
->>>>>>> 0a02c0f (Feature3: update main.py)
 app.add_middleware(RequestContextMiddleware)
 
 # 🔹 Routes
